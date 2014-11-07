ec3ff6856328e850125e3510b4c2cc9c64d5d365
cjh26 <chillar@msri.org>
2014-11-07 19:58:48 +0100

    test_sampling

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -49,14 +49,14 @@ class TestSampling(unittest.TestCase):
         self.assertTrue((J.sum(axis=1).argsort() == sample_from_ising(J,
                             theta, num_samples=10000).mean(axis=1).argsort()).sum() >= 2)
 
-        
+
     def test_dichotomous(self):
         np.random.seed(42)
         mu = np.array([.4, .3])
         cov = np.array([[.24, .1], [.1, .21]])
 
-        X = sample_from_dichotomized_gaussian(mu, cov, 1000)
+        X = sample_from_dichotomized_gaussian(mu, cov, 1000)[0]
         X.mean(axis=1)
 
-        self.assertTrue(np.sum(X.mean(axis=1) - np.array([ 0.396,  0.307])) < 1-5)
-        self.assertTrue(np.sum(X.var(axis=1) - np.array([ 0.239184,  0.212751])) < 1-5)
+        self.assertTrue(np.sum(X.mean(axis=1) - np.array([ 0.396,  0.307])) < 1e-5)
+        self.assertTrue(np.sum(X.var(axis=1) - np.array([ 0.239184,  0.212751])) < 1e-5)
